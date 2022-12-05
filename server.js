caa1a6e5e88f4af661a48ea98a0114efaf1c034b
Anit-Kumar-Singh <anit.kumar@zensar.com>
2022-12-05 09:27:39 +0530

    simple server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const  cors = require('cors');
+const axios = require('axios');
+const PORT = process.env.PORT || 4000
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  app.post('/graphql', (req, res) => {
+    console.log('REquest', req.body)
+    const fetchReq = axios.post(
+        'https://integration-5ojmyuq-kaprizac2nszu.ap-4.magentosite.cloud/graphql',  req.body,
+          {
+            headers: {
+                 'Content-Type': 'application/json'
+            }
+          }
+    ).then(response => {
+        console.log("Response", response.headers['content-type'],response.data)
+        res.send(response.data)
+    }).catch(error => {
+        console.error("Error", error)
+        res.send({"error": "Error"})
+    })
+
+   
+  })
+app.listen(PORT,()=>console.log('Server runiing'));
